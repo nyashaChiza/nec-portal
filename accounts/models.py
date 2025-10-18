1d77567663f3088b8e56a9ef242b755ce42346a5
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-10-18 22:37:37 +0400

    wip

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
-        ('ADMIN', 'Admin'),
-        ('FARMER_MANAGER', 'Farmer Manager'),
-        ('DA', 'Designated Agent'),
-        ('ACCOUNTANT', 'Accountant'),
+        ('Admin', 'Admin'),
+        ('Manager', 'Manager'),
+        ('Designated Agent', 'Designated Agent'),
+        ('Accountant', 'Accountant'),
     ]
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='FARMER_MANAGER')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Manager')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30) 
 
     def __str__(self):
-        return f"{self.username})"
+        return f"{self.username}"
--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('optimus/', admin.site.urls),
-    # path('', RedirectView.as_view(url='/auth/login/', permanent=False)),
+    path('', RedirectView.as_view(url='/auth/login/', permanent=False)),
     path('auth/', include('allauth.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
